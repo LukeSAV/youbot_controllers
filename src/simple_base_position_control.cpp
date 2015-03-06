3ae1861e68932055c502c071a177604be8f99715
stewess <islers@student.ethz.ch>
2015-03-06 13:23:02 +0100

    licensing updates

--- src/simple_base_position_control.cpp
+++ src/simple_base_position_control.cpp
@@ -1,1 +1,17 @@
- 
+
+/* Copyright (c) 2014,2015 , Stefan Isler, islerstefan@bluewin.ch
+*
+This file is part of youbot_controllers, a ROS package providing some additonal controllers for the KUKA youbot,
+
+youbot_controllers is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+youbot_controllers is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with youbot_controllers. If not, see <http://www.gnu.org/licenses/>.
+*/
+
--- src/youbot_arm_controller.cpp
+++ src/youbot_arm_controller.cpp
@@ -1,19 +1,24 @@
- 
- 
-/******************************************************************************
-* Copyright (c) 2014
+/* Copyright (c) 2014,2015 , Stefan Isler, islerstefan@bluewin.ch
 *
-* Author:
-* Stefan Isler
-*
-*
-* Class provides an interface for forward controlling of the youbot arm.
-*
-******************************************************************************/
+This file is part of youbot_controllers, a ROS package providing some additonal controllers for the KUKA youbot,
+
+youbot_controllers is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+youbot_controllers is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with youbot_controllers. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "youbot_controllers/youbot_arm_controller.h"
-
-
+ 
+/** Class provides an interface for forward controlling of the youbot arm.
+*/
 YoubotArmController::YoubotArmController( ros::NodeHandle* _n )
 {
   cout<<endl<<"Initializing Youbot arm controller.."<<endl;
